0104160718b3ac1f243d7ba570dc0a576d544f37
root <root@ubuntu.myguest.virtualbox.org>
2024-01-21 19:50:34 +0800

    test -[hrw]

--- scull/user/test.c
+++ scull/user/test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h> // atoi()
 
 // open()
 #include <sys/types.h>
@@ -9,7 +10,7 @@
 
 int main(int argc, char **argv)
 {
-	char buf[5]="xxxxx";
+	char buf[65536]={'^'};
 
 	int fd = open("/dev/scull0", O_RDWR);
 	if(fd == -1)
@@ -18,15 +19,29 @@ int main(int argc, char **argv)
 	if(argv[1]) {
 		switch(argv[1][1]){
 			case 'r':
-				read(fd, buf, 5);
+				read(fd, buf, atoi(argv[2]));
 				printf("%s\n", buf);
 
 				break;
 			case 'w':
 				if(!argv[2]) return 0;
-				write(fd, argv[2], 5);
+				write(fd, argv[2], atoi(argv[3]));
 
 				break;
+			case 'h':
+				printf(	"NAME\n"
+					"\t test /dev/scull\n"
+
+					"SYNOPSIS\n"
+					"\t test -r COUNTS\n"
+					"\t test -w STRING COUNTS\n"
+
+					"DESCRIPTION\n"
+					"\t-r COUNTS \n"
+					"\t\t COUNTS bytes will be read.\n"
+					"\t-w STRING COUNTS\n"
+					"\t\tCOUNTS bytes of the STRING will be write into scull"
+				);
 			default:
 				printf("error args\n");
 				break;
