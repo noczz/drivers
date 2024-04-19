808789244fa49347678c018ce75656c3cd604f7d
noczz <you@example.com>
2024-04-19 21:58:13 +0800

    GPIO led already

--- mods/rpi/led.c
+++ mods/rpi/led.c
@@ -34,34 +34,35 @@ struct file_operations led_fops = {
 
 static void __exit led_cleanup(void)
 {
+	int val = 0;
+	struct led_dev *dev = &led_device;
+
 	printk(KERN_ALERT "LED clean up.\n");
 
-	struct led_dev *dev = &led_device;
-	dev_t devno = MKDEV(major, minor);
+	// GPIO operations
+
+	/* Clean GPIO pins values */
+	val |= (SET_GPCLR(2, 1) | SET_GPCLR(3, 1) | SET_GPCLR(4, 1));
+	iowrite32(val, gpio_base + GPIO_CLR0_OFFSET);
 
 	/* Clean and Del cdev */
 	cdev_del(&dev->cdev);
 
-	PDEBUG("major %d, minor %d\n", major, minor);
+	/* Unregister*/
+	PDEBUG("Unregistered major %d, minor %d\n", major, minor);
+	dev_t devno = MKDEV(major, minor);
 	unregister_chrdev_region(devno, nr);
 
+	/* Unremap I/O memory*/
 	iounmap(gpio_base);
 	PDEBUG("gpio_base %p released\n", gpio_base);
 
+	/* Release I/O memory */
 	release_mem_region(gpio_phys, GPIO_WIDTH);
-	PDEBUG("we released the region 0x%lX\n", gpio_phys);
+	PDEBUG("Released the region 0x%lX\n", gpio_phys);
 
-//	Clean GPIO value
-//	int val;
-//	val = ~(MASK_CLR(2) | MASK_CLR(3) | MASK_CLR(4));
-//	val &= ioread32(gpio + GPIO_CLR0_OFFSET);
-//	PDEBUG("GPCLR0 0x%X\n", val);
-//
-//	val |= (SET_GPCLR(2, 1) | SET_GPCLR(3, 1) | SET_GPCLR(4, 1));
-//	iowrite32(val, gpio + GPIO_CLR0_OFFSET);
-//
 //	val = -1;
-//	val &= ioread32(gpio + GPIO_CLR0_OFFSET);
+//	val &= ioread32(gpio_base + GPIO_CLR0_OFFSET);
 //	PDEBUG("GPCLR0 0x%X\n", val);
 	return;
 }
@@ -80,13 +81,13 @@ static int __init led_init(void)
 		return -ENODEV;
 	}
 
-	PDEBUG("We got request a memory region 0x%lX\n", gpio_phys);
+	PDEBUG("Got request a memory region 0x%lX\n", gpio_phys);
 
 	gpio_base = ioremap(gpio_phys, GPIO_WIDTH);
 
-	PDEBUG("We got gpio_base %p\n", gpio_base);
+	PDEBUG("Got gpio_base %p\n", gpio_base);
 
-	/* Register led device */
+	/* Register device number */
 	if (major) {
 		devno = MKDEV(major, minor);
 		register_chrdev_region(devno, nr, "led");
@@ -98,7 +99,7 @@ static int __init led_init(void)
 		PDEBUG("Can't get major %d\n", major);
 		return result;
 	}
-	PDEBUG("major %d, minor %d\n", major, minor);
+	PDEBUG("Registered at major %d, minor %d\n", major, minor);
 
 	/* Init and Add cdev */
 	cdev_init(&dev->cdev, &led_fops);
@@ -109,34 +110,28 @@ static int __init led_init(void)
 		goto fail;
 	}
 
-	// LED open
-//	int val;
+	// LED Operations
+	int val = 0;
 
-//	val = ~(MASK_FSEL(2) | MASK_FSEL(3) | MASK_FSEL(4));
-//	val &= ioread32(gpio + GPIO_FSEL0_OFFSET);
-//	PDEBUG("GPFSEL0 0x%0X\n", val);
-//
-//	// Set up GPIO to out mode
-//	val |= (SET_GPFSEL(2, GPOUT) |
-//		SET_GPFSEL(3, GPOUT) |
-//		SET_GPFSEL(4, GPOUT));
-//	iowrite32(val, gpio + GPIO_FSEL0_OFFSET);
-//
 //	val = -1;
-//	val &= ioread32(gpio + GPIO_FSEL0_OFFSET);
+//	val &= ioread32(gpio_base + GPIO_FSEL0_OFFSET);
 //	PDEBUG("GPFSEL0 0x%0X\n", val);
-//
-//	// Set GPIO value
-//	val = ~(MASK_SET(2) | MASK_SET(3) | MASK_SET(4));
-//	val &= ioread32(gpio + GPIO_SET0_OFFSET);
-//	PDEBUG("GPSET0 0x%0X\n", val);
-//
-//	val |= (SET_GPSET(2, 1) | SET_GPSET(3, 1) | SET_GPSET(4, 1));
-//	PDEBUG("GPSET0 0x%0X\n", val);
-//	iowrite32(val, gpio + GPIO_SET0_OFFSET);
-//
+
+	// Set GPIO pins to out mode
+	val |= (SET_GPFSEL(2, GPOUT) |
+		SET_GPFSEL(3, GPOUT) |
+		SET_GPFSEL(4, GPOUT));
+	iowrite32(val, gpio_base + GPIO_FSEL0_OFFSET);
+
+	// Set GPIO pins value
+	val = ~(MASK_SET(2) | MASK_SET(3) | MASK_SET(4));
+
+	val |= (SET_GPSET(2, 1) | SET_GPSET(3, 1) | SET_GPSET(4, 1));
+	PDEBUG("GPSET0 0x%0X\n", val);
+	iowrite32(val, gpio_base + GPIO_SET0_OFFSET);
+
 //	val = -1;
-//	val &= ioread32(gpio + GPIO_SET0_OFFSET);
+//	val &= ioread32(gpio_base + GPIO_SET0_OFFSET);
 //	PDEBUG("GPSET0 0x%0X\n", val);
 
 	return 0;
